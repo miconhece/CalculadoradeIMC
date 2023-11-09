246c510bd07d75c3787111dba33138e641de3266
miconhece <santosmichele.contato@gmail.com>
2023-11-09 12:31:09 -0300

    IMP: Add statusbar

--- app/src/main/java/com/pucpr/calculadoradeimc/MainActivity.java
+++ app/src/main/java/com/pucpr/calculadoradeimc/MainActivity.java
@@ -1,8 +1,12 @@
 package com.pucpr.calculadoradeimc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorDark));
     }
 }
